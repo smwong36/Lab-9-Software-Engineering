d861c59c559ed8c9b11418f29c9af4198c42d9a2
Sam Wong <samanthawong.smw@gmail.com>
2024-07-27 15:02:38 -0400

    final edit

--- Lab_9.py
+++ Lab_9.py
@@ -1,5 +1,6 @@
+
 def encode(password):
-# Takes a string of numbers, and returns a string of the numbers after adding 3.
+# This function is written by Samantha Wong
     new_string =""
     for i in range(len(password)):
         x = int(password[i]) +3
@@ -14,3 +15,35 @@ def decode(password):
         digit = chr(ord(i) - 3)
         decoded_str.append(digit)
     return ''.join(decoded_str)
+
+def main(): # This function is written by Samantha Wong
+    
+    encrypt = True
+    while encrypt:
+    # print menu
+        print("Menu")
+        print('-------------')
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+    # input option
+        option = input("Please enter an option: ")
+    # encode
+        if int(option) == 1:
+            password = input('Please enter the password to encode: ')
+            encoded_pass = encode(password)
+            print("Your password has been encoded and stored!")
+            print()
+    # decode
+        elif int(option) == 2:
+            decoded_pass = decode(encoded_pass)
+            print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
+            print()
+    # quit
+        elif int(option) == 3:
+            encrypt = False
+            
+            
+if __name__ == "__main__":
+    main()
